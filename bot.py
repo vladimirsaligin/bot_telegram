3a8c37ad1f435d63fa960204b1c0c652cf76c473
vladimirsaligin <saliginvladimir@gmail.com>
2023-05-11 20:00:59 +0300

    новое

--- bot.py
+++ bot.py
@@ -1,5 +1,23 @@
-print('привет !')
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler
+from telegram.ext import filters
 
-#собираем телеграм бота
-print('ывсывс'
-      '')
+# Создаем экземпляр бота
+bot = telegram.Bot(token='5987197501:AAHvadKeHvZqX_gOq3H_v-9p0vV_rra-lsQ')
+
+# Обработчик команд
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Привет, я бот!")
+
+# Обработчик текстовых сообщений
+def echo(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+# Создаем экземпляр обновления и регистрируем обработчики
+updater = Updater(use_context=True)
+dispatcher = updater.dispatcher
+dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(MessageHandler(filters.text & ~filters.command, echo))
+
+# Запускаем бота
+updater.start_polling(token='5987197501:AAHvadKeHvZqX_gOq3H_v-9p0vV_rra-lsQ')
